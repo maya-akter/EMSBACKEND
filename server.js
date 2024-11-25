87a3779301d9bbdda046bfe375a86f7efb225e8f
maya <9999mayaakter@gmail.com>
2024-11-25 21:52:28 +0600

    dfsdfs

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ const app = express();
 
 
 
-app.use(cors());
+app.use(cors({
+    origin:"https://emsfrontend-theta.vercel.app",
+    credentials:true,
+}));
 app.use(express.json());
 app.use(express.static('public/uploads'))
 
